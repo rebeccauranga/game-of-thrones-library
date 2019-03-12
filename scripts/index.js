d30e4943be84f570daa45b072c9374959231abf7
Rebecca Uranga <urangarebecca@gmail.com>
2019-03-12 11:36:20 -0400

    sorts characters and draws them to listing

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let allCharactersArray = [];
 
-function urlForPage(pageNumber = 0) {
+function urlForPage(pageNumber=0) {
   return `http://my-little-cors-proxy.herokuapp.com/https://anapioficeandfire.com/api/characters/?page=${pageNumber}&pageSize=50`;
 }
 
@@ -24,9 +24,13 @@ function loadCharacters() {
   const jsonCharacters = localStorage.getItem(storageKey);
   // convert it back to an array
   const arrayOfCharacters = JSON.parse(jsonCharacters);
-  console.log(` loaded ${arrayOfCharacters.length} characters`);
-  // return it
-  return arrayOfCharacters;
+  if (arrayOfCharacters) {
+      console.log(`loaded ${arrayOfCharacters.length} characters`);
+  } else {
+    console.log('No characters in localStorage');
+    }
+ // return it
+    return arrayOfCharacters;
 }
 
 function retrievePageOfCharacters(pageNumber){
@@ -40,9 +44,60 @@ function retrievePageOfCharacters(pageNumber){
       });
 }
 
-for (let pageNumber = 0; pageNumber < 43; pageNumber++) {
-    let delay  = pageNumber * 500;
-    setTimeout(function () {
-        retrievePageOfCharacters(pageNumber);
-    }, delay);
+function drawSingleCharacterToListing(characterObject){
+    const characterName = characterObject.name;
+    if (characterName.length === 0) {
+        return;
+    }
+    const anchorElement = document.createElement('a');
+    anchorElement.textContent = characterName;
+
+    const listItem = document.createElement('li');
+    listItem.appendChild(anchorElement);
+
+    const listArea = document.querySelector('[data-listing]');
+    listArea.appendChild(listItem);
 }
+
+function drawListOfCharacters() {
+
+
+    allCharactersArray.forEach(drawSingleCharacterToListing);
+}
+
+function sortByName(obj1, obj2){
+    const letter1 = obj1.name[0];
+    const letter2 = obj2.name[0];
+
+    if (letter1 < letter2) {
+
+        return -1;
+    } else if (letter2 < letter1) {
+
+        return 1;
+    }
+
+    return 0;
+}
+
+function main() {
+    let charactersInLocalStorage = loadCharacters();
+    if (charactersInLocalStorage){
+        allCharactersArray = [
+            ...charactersInLocalStorage.sort(sortByName)
+        ];
+        drawListOfCharacters();
+    } else {
+        console.log("You got nothing.");
+        console.log("Retrieving from the API");
+        for (let pageNumber=0; pageNumber<50; pageNumber++) {
+            let delay  = pageNumber * 500;
+            setTimeout(function () {
+                retrievePageOfCharacters(pageNumber);
+            }, delay);
+        }
+    }
+}
+
+main();
+
